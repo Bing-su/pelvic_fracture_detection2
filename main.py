9f3ea4d02d54997f5f74073c8d32ea75141e8428
Dowon <ks2515@naver.com>
2022-07-14 17:15:41 +0900

    fix: typer, pre-commit config

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pandas as pd
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, RichProgressBar
@@ -13,18 +15,41 @@ cli = Typer()
 @cli.command()
 def train(
     model_name: str = Option(
-        "vit_tiny_patch16_224", "--model-name", "-m", help="Model name"
+        "densenet121",
+        "--model-name",
+        "-m",
+        help="Model name",
+        rich_help_panel="Model",
     ),
     optimizer_name: str = Option(
-        "madgradw", "--optimizer-name", "-o", help="Optimizer name"
+        "madgradw",
+        "--optimizer-name",
+        "-o",
+        help="Optimizer name",
+        rich_help_panel="Model",
     ),
-    learning_rate: float = Option(1e-4, "--learning-rate", "-l", help="Learning rate"),
-    weight_decay: float = Option(1e-4, "--weight-decay", "-w", help="Weight decay"),
-    epochs: int = Option(5, "--epochs", "-e", min=1, help="Total epochs"),
-    batch_size: int = Option(32, "--batch-size", "-b", min=1, help="Batch size"),
-    auto_lr_find: bool = Option(False, help="Auto learning rate find"),
-    img_size: int = Option(512, "--img-size", "-i", help="Input image size"),
+    learning_rate: float = Option(
+        1e-3, "--learning-rate", "-l", help="Learning rate", rich_help_panel="Training"
+    ),
+    weight_decay: float = Option(
+        1e-4, "--weight-decay", "-w", help="Weight decay", rich_help_panel="Training"
+    ),
+    epochs: int = Option(
+        5, "--epochs", "-e", min=1, help="Total epochs", rich_help_panel="Training"
+    ),
+    batch_size: int = Option(
+        32, "--batch-size", "-b", min=1, help="Batch size", rich_help_panel="Training"
+    ),
+    auto_lr_find: bool = Option(
+        False, help="Auto learning rate find", rich_help_panel="Training"
+    ),
+    img_size: int = Option(
+        512, "--img-size", "-i", help="Input image size", rich_help_panel="Model"
+    ),
     data_path: str = Option("data/data.csv", "--data-path", "-d", help="Data path"),
+    project_name: Optional[str] = Option(
+        None, "--project-name", "-p", help="Project name"
+    ),
 ):
     model = ImageModel(
         model_name=model_name,
@@ -37,7 +62,10 @@ def train(
     used_data = "mura" if "mura" in data_path else "default"
     datamodule = ImageDataModule(df, img_size=img_size, batch_size=batch_size)
 
-    logger = WandbLogger(name="pelvic-fracture-detection-auroc")
+    if not project_name:
+        project_name = f"{model_name}_{used_data}"
+
+    logger = WandbLogger(name=project_name)
     checkpoints = ModelCheckpoint(
         monitor="val_AUROC",
         mode="max",
@@ -47,7 +75,7 @@ def train(
     trainer = pl.Trainer(
         accelerator="auto",
         logger=logger,
-        log_every_n_steps=20,
+        log_every_n_steps=30,
         gpus=1,
         callbacks=[RichProgressBar(), checkpoints],
         precision=16,
