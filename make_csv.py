60cbe3893d6c8866c6509835584db198b4be66fb
dowon <ks2515@naver.com>
2022-09-15 07:27:01 +0000

    fix: fix make csv

--- make_csv.py
+++ make_csv.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 
 import pandas as pd
+from loguru import logger
 from typer import Typer
 
 cli = Typer()
 
 
-@cli.command()
-def ai_voucher_only() -> None:
-    path = Path.home() / "data"
+@cli.command(no_args_is_help=True)
+def make_csv(root: str) -> None:
+    path = Path(root)
 
     image = []
     label = []
@@ -18,32 +19,14 @@ def ai_voucher_only() -> None:
             y = 1
         else:
             y = 0
-        image.append(img)
+        image.append(str(img.resolve()))
         label.append(y)
 
     df = pd.DataFrame({"image": image, "label": label})
+    logger.debug(f"num of data: {len(df)}")
     Path("data").mkdir(exist_ok=True)
     df.to_csv("data/data.csv", index=False)
-
-
-@cli.command()
-def with_MURA() -> None:
-    path = Path.home() / "MURA"
-
-    image = []
-    label = []
-
-    for img in path.rglob("*.png"):
-        if "abnormal" in str(img):
-            y = 1
-        else:
-            y = 0
-        image.append(img)
-        label.append(y)
-
-    df = pd.DataFrame({"image": image, "label": label})
-    Path("data").mkdir(exist_ok=True)
-    df.to_csv("data/mura.csv", index=False)
+    logger.info(f"data saved at: {Path('data/data.csv').resolve()}")
 
 
 if __name__ == "__main__":
