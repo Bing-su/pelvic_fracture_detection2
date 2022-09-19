8844327f8f6a8a51c8b121d65de16b5e5229a456
dowon <ks2515@naver.com>
2022-09-19 07:16:19 +0000

    feat: shell files

---
+++ run.sh
@@ -0,0 +1,5 @@
+python main.py \
+    mobilevitv2_100 \
+    --epochs 100 \
+    --batch-size 32 \
+    --learning-rate 5e-3
---
+++ run_test.sh
@@ -0,0 +1,3 @@
+python main.py \
+    mobilevitv2_200 \
+    --fast-dev-run
